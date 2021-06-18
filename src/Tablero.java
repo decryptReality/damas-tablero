5e44718630e764263e9000b899a4a7c19e417b34
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-18 13:20:40 -0600

    fila mejorada (3)

--- src/Tablero.java
+++ src/Tablero.java
@@ -3,7 +3,8 @@ class Tablero
 {
     public static void main(String[] args) 
     {
-        Tablero.imprimirFila(1, 8);
+        //Tablero.imprimirFila(1, 8);
+        Tablero.imprimirTablero(5, 3, 7, 1);
     }
 
     public static final String RESET = "\033[0m";  // Text Reset
@@ -16,12 +17,25 @@ class Tablero
     public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
     public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
 
-    static void imprimirFila(int celda1, int celda2) // [1,8]
-    {
+    static void imprimirFila(boolean esCeldaInicialBlanca, int celda1, int celda2) // con fichas: [1,8], sin fichas: otro numero
+    { 
         String colorRESET = "\033[0m";
+        // colores de columnas
+        String colorNoPAR = "\033[47m"; // con este se inicia, columna 1: NoPar
         String colorPAR = "\033[46m";
-        String colorNoPAR = "\033[47m";
+        // color de ficha
         String colorFichaA = "\033[41m";
+
+        if(esCeldaInicialBlanca)
+        {
+            colorNoPAR = "\033[47m";
+            colorPAR = "\033[46m";
+        }
+        else
+        {
+            colorNoPAR = "\033[46m";
+            colorPAR = "\033[47m";
+        }
 
         // fila A
         int i = 3;
@@ -42,7 +56,7 @@ class Tablero
             }
             else
             {
-                if(i % 2 != 0) // SI no es par, o sea, impar, entonces es blanco superior izquierda: blanco
+                if(i % 2 != 0) // SI es NoPar
                 {
                     System.out.print(colorNoPAR + "   " + colorRESET);
                 }
@@ -97,7 +111,7 @@ class Tablero
             }
             else
             {
-                if(i % 2 != 0) // SI no es par, o sea, impar, entonces es blanco superior izquierda: blanco
+                if(i % 2 != 0)
                 {
                     System.out.print(colorNoPAR + "   " + colorRESET);
                 }
@@ -109,6 +123,37 @@ class Tablero
             i = i + 3;
         }
         System.out.println();
+    }
 
+    static void imprimirTablero(int fila1, int columna1, int fila2, int columna2)
+    {
+        int i = 1;
+        while(i <= 8)
+        { 
+            boolean esCeldaInicialBlanca = i % 2 != 0;
+            if(fila1 == i & fila2 == i)  
+            { 
+                imprimirFila(esCeldaInicialBlanca, columna1, columna2);
+            }
+            else
+            {
+                if(fila1 != i & fila2 != i)
+                {
+                    imprimirFila(esCeldaInicialBlanca, 0, 0);
+                }
+                else
+                {
+                    if(fila1 == i)
+                    {
+                        imprimirFila(esCeldaInicialBlanca, columna1, 0);
+                    }
+                    else
+                    {
+                        imprimirFila(esCeldaInicialBlanca, 0, columna2);
+                    }
+                }
+            }
+            i = i + 1;
+        }
     }
 }
